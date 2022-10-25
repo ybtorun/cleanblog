e3b604642f527a713ee3424613e7fc7bbe74d0a1
Yusuf Büyüktorun <ybuyuktoruntest@gmail.com>
2022-10-25 15:20:54 +0300

    added mongodb connection and post

--- app.js
+++ app.js
@@ -1,8 +1,66 @@
+//3. party modules
 const express = require('express');
-const app = express();
-const ejs = require('ejs');
+const mongoose = require('mongoose');
 
-const path =require('path');
+//core modules
+const ejs = require('ejs');
+const path = require('path');
+
+//modules created by us
+const Post = require('./models/Post');
+
+const app = express();
+
+//connect db
+mongoose.connect('mongodb://127.0.0.1:27017/cleanblog-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+//Middlewares
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.set('view engine', 'ejs'); //express template engine olarak ejs kullanacağız diyoruz.
+
+//ROUTES
+app.get('/', async (req, res) => {
+  const posts = await Post.find({})
+  res.render('index', {
+    posts
+  });
+});
+
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add_post', (req, res) => {
+  console.log(req.body);
+  res.render('add_post');
+});
+
+app.get('/post', (req, res) => {
+  res.render('post');
+});
+
+app.post('/posts', async (req, res) => {
+  console.log(req.body);
+  await Post.create(req.body);
+  res.redirect('/');
+});
+
+
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı. `);
+});
+
+
 
 //1. homework
 // app.get('/', (req, res) => {
@@ -10,30 +68,6 @@ const path =require('path');
 //   res.send(blog);
 // });
 
-app.use(express.static('public'));//Middlewares
-
 // app.get('/', (req, res) => {
 //   res.sendFile(path.resolve(__dirname,'temp/index.html'));
 // });
-
-app.set('view engine', 'ejs');  //express template engine olarak ejs kullanacağız diyoruz.
-
-app.get('/', (req, res) => {
-  res.render('index');
-});
-
-app.get('/about', (req, res) => {
-  res.render('about');
-});
-
-app.get('/add_post', (req, res) => {
-  res.render('add_post');
-});
-
-app.get('/post', (req, res) => {
-  res.render('post');
-});
-const port = 3000;
-app.listen(port, () => {
-  console.log(`Sunucu ${port} portunda başlatıldı. `);
-});
